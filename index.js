f278d1501165e23ba4101b49d1ae92b8c05e0560
Iryna Predborska <iryna.predborska@gmail.com>
2020-04-18 19:13:02 +0200

    add initial logic on our server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+let today = new Date();
+let  currentDay = today.getDay();
+
+if (currentDay === 6 || currentDay === 0) {
+    res.send("It's the weekend! Awesome!");
+} else {
+    res.send("It's a weekday...ohhh!");
+}
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000.")
+});
