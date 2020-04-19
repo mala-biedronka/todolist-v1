6c088ef2f68ca3ef07b3315e6816f357d5da34b2
Iryna Predborska <iryna.predborska@gmail.com>
2020-04-19 10:13:21 +0200

    install ejs module

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
