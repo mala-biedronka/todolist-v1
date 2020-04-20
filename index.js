c2be64b559fb3b735ab818da9b070259c00ae959
Iryna Predborska <iryna.predborska@gmail.com>
2020-04-20 13:10:04 +0200

    swipe a switch statement to toLocaleDateString()

--- index.js
+++ index.js
@@ -9,34 +9,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default: console.log("Surprise! There is one plus new day in the week!");
-    }
-    res.render("list", {kindOfDay: day});
+    let options = {
+        weekday: "long",
+        month: "long",
+        day: "numeric"
+    };
+
+    let currentDate = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: currentDate, newItem: usersInput});
+});
+
+app.post ("/", function (req, res) {
+    let usersInput = req.body.newListItem;
+    res.redirect("/");
 });
 
 app.listen(3000, function () {
